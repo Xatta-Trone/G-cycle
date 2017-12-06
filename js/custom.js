090c4fe4c4d47b8641713e0ec78efe4de3a4bf0f
Monzurul Islam <monzurul.ce.buet@gmail.com>
2017-12-06 21:49:51 +0600

    typed fix

--- js/custom.js
+++ js/custom.js
@@ -60,24 +60,14 @@ document.addEventListener('DOMContentLoaded', function() {
     backSpeed: 20,
     startDelay: 1000,
     loop: true,
-    loopCount: Infinity,
-    onComplete: function(self) { prettyLog('onComplete ' + self) },
-    preStringTyped: function(pos, self) { prettyLog('preStringTyped ' + pos + ' ' + self); },
-    onStringTyped: function(pos, self) { prettyLog('onStringTyped ' + pos + ' ' + self) },
-    onLastStringBackspaced: function(self) { prettyLog('onLastStringBackspaced ' + self) },
-    onTypingPaused: function(pos, self) { prettyLog('onTypingPaused ' + pos + ' ' + self) },
-    onTypingResumed: function(pos, self) { prettyLog('onTypingResumed ' + pos + ' ' + self) },
-    onReset: function(self) { prettyLog('onReset ' + self) },
-    onStop: function(pos, self) { prettyLog('onStop ' + pos + ' ' + self) },
-    onStart: function(pos, self) { prettyLog('onStart ' + pos + ' ' + self) },
-    onDestroy: function(self) { prettyLog('onDestroy ' + self) }
+    loopCount: Infinity
   });
 
   
 });
 
 function prettyLog(str) {
-  console.log('%c ' + str, 'color: green; font-weight: bold;');
+  //console.log('%c ' + str, 'color: green; font-weight: bold;');
 }
 
 function toggleLoop(typed) {
